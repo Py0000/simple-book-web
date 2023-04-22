db94ba01e25537648c91b82a3cd88973ac302ef8
Py0000 <pylim8266@gmail.com>
2023-04-22 15:45:07 +0800

    Add logic to retrieve all books from database.

--- backend/database.js
+++ backend/database.js
@@ -1,28 +1,30 @@
 const mysql = require("mysql");
 
-const hostName = "127.0.0.1";
-const userName = "root";
-const password = "";
-const databaseName = "simple-book-web";
-const databasePort = 3306
+const HOST_NAME = "127.0.0.1";
+const USER_NAME = "root";
+const PASSWORD = "";
+const DB_NAME = "simple-book-web";
+const DB_PORT = 3306
 
-const connectionErrorMessage = `Error occured when attempting to connect to ${databaseName} database.`;
-const connectionSuccessMessage = `Successfully establish connection to ${databaseName} database.`;
+
+
 
 // Set up the database needed for the application
 const db = mysql.createConnection({
-    host: hostName,
-    user: userName,
-    password: password,
-    database: databaseName,
-    port: databasePort
+    host: HOST_NAME,
+    user: USER_NAME,
+    password: PASSWORD,
+    database: DB_NAME,
+    port: DB_PORT
 });
 
 db.connect((error) => {
     if (error) {
-        console.log(connectionErrorMessage + error);
+        const connectionErrMsg = `Error occured when attempting to connect to ${DB_NAME} database. \n`;
+        console.log(connectionErrMsg + error);
     } else {
-        console.log(connectionSuccessMessage);
+        const connectionSuccessMsg = `Successfully establish connection to ${DB_NAME} database.`;
+        console.log(connectionSuccessMsg);
     }
 });
 
--- backend/main.js
+++ backend/main.js
@@ -1,8 +1,8 @@
 
-const portNumber = 9000;
-const setupPortMessage = `Backend setup, running on port ${portNumber}`;
+const BACKEND_PORT_NUM = 9000;
 
-const main_backend_server_tag = "/";
+const MAIN_BACKEND_TAG = "/";
+const LIST_TAG = "/books";
 
 
 // Creates the main app
@@ -16,19 +16,34 @@ const cors = require("cors");
 app.use(cors());
 
 // Listen to a port number
-app.listen(portNumber, () => {
+app.listen(BACKEND_PORT_NUM, () => {
     // Confirmation log message
-    console.log(setupPortMessage);
+    const listenPortSuccessMsg = `Backend setup, running on port ${BACKEND_PORT_NUM}`;
+    console.log(listenPortSuccessMsg);
 })
 
 
 // Create and setup database
-const database = require("./database");
+const db = require("./database");
 
 
 // Reaching the backend server via API request (using express)
-app.get(main_backend_server_tag, (request, response) => {
+app.get(MAIN_BACKEND_TAG, (request, response) => {
     response.json("Backend Server reached");
+});
+
+
+app.get(LIST_TAG, (request, response) => {
+    const query = "SELECT * FROM books";
+
+    db.query(query, (error, data) => {
+        if (error) {
+            const retrieveBooksErrMsg = "Error retrieving all books. \n";
+            return response.json(retrieveBooksErrMsg + error);
+        } 
+
+        return response.json(data);
+    });
 });
 
 
