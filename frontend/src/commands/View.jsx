1e582494f50ef1f21b325f173d4d987ff1ff02c3
Py0000 <pylim8266@gmail.com>
2023-04-22 16:33:03 +0800

    Add delete button to each book

--- frontend/src/commands/View.jsx
+++ frontend/src/commands/View.jsx
@@ -6,6 +6,8 @@ const View = () => {
     const path = "http://localhost:9000/books";
     const [books, displayBooks] = useState([]);
 
+
+    // Listing all books in database
     useEffect(() => {
         // Calling API, needs to be async
         // Allows streaming of data and sending of multiple request at the same time
@@ -13,7 +15,6 @@ const View = () => {
             try {
                 // need to await as it is an async function
                 const result = await axios.get(path);
-                console.log(result.data)
                 displayBooks(result.data)
             }
 
@@ -25,6 +26,18 @@ const View = () => {
 
         getBooks();
     }, []);
+
+
+    // Deleting book from database
+    const handleDelete = async (id)=> {
+        try {
+            await axios.delete(path + id);
+            window.location.reload();
+        }
+        catch (err) {
+            console.log(err);
+        }
+    }
     
 
     return (
@@ -37,6 +50,7 @@ const View = () => {
                         <p>{book.publisher}</p>
                         <p>{book.year}</p>
                         <p>{book.authorId}</p>
+                        <button className='deleteButton' onClick={()=>handleDelete(book.id)}>Delete</button>
                     </div>    
                 ))}
             </div>
