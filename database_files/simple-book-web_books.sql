99df6e499866aaba4e59459ea49d07441703398b
Py0000 <pylim8266@gmail.com>
2023-04-24 22:07:16 +0800

    Revise database state

--- database_files/simple-book-web_books.sql
+++ database_files/simple-book-web_books.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `simple-book-web` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `simple-book-web`;
 -- MySQL dump 10.13  Distrib 8.0.33, for Win64 (x86_64)
 --
 -- Host: localhost    Database: simple-book-web
@@ -51,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-24 21:50:23
+-- Dump completed on 2023-04-24 22:05:11
