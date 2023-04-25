bea2f29a86322904c18f8e5591d794ce352a49a1
Py0000 <pylim8266@gmail.com>
2023-04-25 17:27:00 +0800

    refactor part 1

--- frontend/src/commands/authors/AddAuthor.jsx
+++ frontend/src/commands/authors/AddAuthor.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import {validateText, validateBiography} from '../../utils/ValidationLogic';
+import * as frontendConstant from '../../utils/FrontendUtils';
 
 import classes from '../../ui/page_styles/Form.module.css';
 import BackButton from '../../ui/buttons/BackButton';
@@ -11,8 +12,6 @@ import '../../ui/page_styles/AddAuthor.css';
 
 
 const AddAuthor = () => {
-    const path = "http://localhost:9000/authors";
-
     const [author, setAuthor] = useState({
         name: "",
         biography: ""
@@ -21,21 +20,6 @@ const AddAuthor = () => {
     const [error, setError] = useState();
     
     const [status, setStatus] = useState();
-
-    const isValidEmpty = () => {
-        let isNameEmpty = author.name.trim().length === 0;
-        let isBiographyEmpty = author.biography.trim().length === 0;
-
-        let isInputValid = !isNameEmpty && !isBiographyEmpty;
-        return isInputValid;
-    }
-
-
-    const isTooLong = (desc, limit) => {
-        let descLength = desc.trim().length;
-        return descLength > limit;
-    }
-
 
     const handleChange = (e) => {
         setAuthor((prev) => (
@@ -46,42 +30,35 @@ const AddAuthor = () => {
 
     const handleClick = async (e) => {
         e.preventDefault();
+
         try {
-            try {
-                // Simple Input validation
-                if (!validateText(author.name)) {
-                    setError({
-                        title: "Invalid Input",
-                        message: "Author name should not be empty and should not have more than 45 characters"
-                    });
-                    return;
-                }
-
-                if (!validateBiography(author.biography)) {
-                    setError({
-                        title: "Invalid Input",
-                        message: "Author biography should not be empty and should not have more than 200 characters"
-                    });
-                    return;
-                }
-
-                // Input valid, add to database
-                let res = await axios.post(path, author);
-                
-                setStatus({
-                    title: "Added Author Status",
-                    message: res.data
+            // Simple Input validation
+            if (!validateText(author.name)) {
+                setError({
+                    title: frontendConstant.ERROR_MODAL_TITLE,
+                    message: frontendConstant.ERROR_AUTHOR_NAME
                 });
-
-            } catch (error) {
-                const errMsg = "[Frontend] Error adding data to backend. \n";
-                console.log(errMsg + error);
+                return;
             }
-        }
 
-        catch (error) {
-            const errIdentifier = "[Frontend] ";
-            console.log(errIdentifier +  error);
+            if (!validateBiography(author.biography)) {
+                setError({
+                    title: frontendConstant.ERROR_MODAL_TITLE,
+                    message: frontendConstant.ERROR_AUTHOR_BIO
+                });
+                return;
+            }
+
+            // Input valid, add to database
+            let res = await axios.post(frontendConstant.AUTHOR_PATH, author);
+            
+            setStatus({
+                title: frontendConstant.STATUS_MODAL_TITLE,
+                message: res.data
+            });
+
+        } catch (error) {
+            console.log(frontendConstant.ERROR_MSG + error);
         }
     };
 
@@ -98,16 +75,16 @@ const AddAuthor = () => {
         <div>
             {error && <Modal title={error.title} message={error.message} handleAction={errorHandler}></Modal>}
             {status && <Modal title={status.title} message={status.message} handleAction={statusHandler}></Modal>}
-            <BackButton><Link to="/view_authors">Back</Link></BackButton>
+            <BackButton><Link to={frontendConstant.VIEW_AUTHOR_LINK}>{frontendConstant.BACK_BUTTON}</Link></BackButton>
             <div className={classes.input}>
-                <h1>Add New Author</h1>
-                <input type="text" placeholder='Enter name here' onChange={handleChange} name="name"></input>
-                <input type="text" placeholder='Enter biography here' onChange={handleChange} name="biography"></input>
-                <FormButton type="submit" onClick={handleClick}>Add Author</FormButton>
-                <FormButton><Link to="/addbook">Save Book's Details Here</Link></FormButton>
+                <h1>{frontendConstant.ADD_AUTHOR_PAGE_TTTLE}</h1>
+                <input type="text" placeholder={frontendConstant.PLACEHOLDER_NAME} onChange={handleChange} name="name"></input>
+                <input type="text" placeholder={frontendConstant.PLACEHOLDER_BIO} onChange={handleChange} name="biography"></input>
+                <FormButton type="submit" onClick={handleClick}>{frontendConstant.ADD_AUTHOR_BUTTON}</FormButton>
+                <FormButton><Link to={frontendConstant.ADD_BOOK_LINK}>{frontendConstant.SAVE_BOOK_DETAILS}</Link></FormButton>
             </div>
             <div className='add-author__remark'>
-                <p>Note: If the author already exists in our database, you won't be able to add it in, hence no visible change will be noticed!</p>
+                <p>{frontendConstant.WARNING_MSG}</p>
             </div>
         </div>
     )
---
+++ frontend/src/utils/FrontendUtils.js
@@ -0,0 +1,29 @@
+// Path for author database
+export const AUTHOR_PATH = "http://localhost:9000/authors";
+
+// Links 
+export const VIEW_AUTHOR_LINK = "/view_authors";
+export const ADD_BOOK_LINK = "/addbook";
+
+// Forms placeholders
+export const PLACEHOLDER_NAME = 'Enter name here';
+export const PLACEHOLDER_BIO = 'Enter biography here';
+
+// Related to Input validation modal
+export const ERROR_MODAL_TITLE = "Invalid Input";
+export const ERROR_AUTHOR_NAME = "Author name should not be empty and should not have more than 45 characters";
+export const ERROR_AUTHOR_BIO = "Author biography should not be empty and should not have more than 200 characters";
+
+
+// Related to status update modal
+export const STATUS_MODAL_TITLE = "Status Update";
+export const ERROR_MSG = "Unexpected Error Occurred. \n";
+
+// Related to Add Author page
+export const ADD_AUTHOR_PAGE_TTTLE = "Add New Author";
+export const ADD_AUTHOR_BUTTON = "Add Author";
+export const SAVE_BOOK_DETAILS = "Save Book's Details Here";
+export const WARNING_MSG = "Note: If the author already exists in our database, you won't be able to add it in, hence no visible change will be noticed!";
+
+// Back button (Used in AddAuthor and AddBook)
+export const BACK_BUTTON = "Back";
